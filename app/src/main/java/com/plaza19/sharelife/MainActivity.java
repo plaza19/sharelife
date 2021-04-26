683333a416fcb53fdd380f3d718323ab93f3c15f
plaza19 <diegoplazarodan19@protonmail.ch>
2021-04-26 22:09:41 +0200

    Firebase authentication + Firestore
    Improvements in the rregister activity, user data is saved in firestore
    and auth.

--- app/src/main/java/com/plaza19/sharelife/MainActivity.java
+++ app/src/main/java/com/plaza19/sharelife/MainActivity.java
@@ -5,16 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewRegistrar;
+    private EditText edit_email, edit_password;
+    private Button btn_sesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit_email = findViewById(R.id.edit_email_main);
+        edit_password = findViewById(R.id.edit_password_main);
+        btn_sesion = findViewById(R.id.btn_iniciarSesion);
 
         textViewRegistrar = findViewById(R.id.texViewRegistrar);
         textViewRegistrar.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, RegistroActivity.class);
                 startActivity(i);
+            }
+        });
+
+        btn_sesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
             }
         });
     }
--- app/src/main/java/com/plaza19/sharelife/RegistroActivity.java
+++ app/src/main/java/com/plaza19/sharelife/RegistroActivity.java
@@ -1,5 +1,6 @@
 package com.plaza19.sharelife;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,10 +12,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
 public class RegistroActivity extends AppCompatActivity {
 
     private EditText edit_user,edit_email, edit_password, edit_password_confirm;
     private Button btn_register;
+    private FirebaseAuth auth;
+    private FirebaseFirestore firestore;
+    private final String COLLECTION_USERS = "Users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,8 @@ public class RegistroActivity extends AppCompatActivity {
         edit_password = findViewById(R.id.edit_password);
         edit_password_confirm = findViewById(R.id.edit_password_confirm);
         btn_register = findViewById(R.id.button_register);
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
 
 
         edit_password_confirm.addTextChangedListener(new TextWatcher() {
@@ -39,15 +53,62 @@ public class RegistroActivity extends AppCompatActivity {
 
                 //comprobamos que el correo sea valido
 
-                if (checkMail() && checkUser() && checkPassword()) {
-                    btn_register.setEnabled(true);
-                    Toast.makeText(RegistroActivity.this, "activo", Toast.LENGTH_SHORT).show();
-                }
+                checkFields();
 
-                Toast.makeText(RegistroActivity.this, "desactivao", Toast.LENGTH_SHORT).show();
-                checkMail();
-                checkPassword();
-                checkUser();
+                //Toast.makeText(RegistroActivity.this, "desactivao", Toast.LENGTH_SHORT).show();
+                //checkMail();
+                //checkPassword();
+                //checkUser();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        edit_email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                checkFields();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        edit_user.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkFields();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        edit_password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkFields();
             }
 
             @Override
@@ -56,16 +117,52 @@ public class RegistroActivity extends AppCompatActivity {
             }
         });
 
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+
+    }
+
+    private void createUser() {
+        auth.createUserWithEmailAndPassword(edit_email.getText().toString(), edit_password_confirm.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(RegistroActivity.this, "Usuario creado correctamente", Toast.LENGTH_SHORT).show();
+                    HashMap<String, Object> data_map = new HashMap<>();
+                    data_map.put("email", edit_email.getText().toString());
+                    data_map.put("user_name", edit_user.getText().toString());
+                    //Documento usuario con el uid que se genera en firebase-auth
+                    firestore.collection(COLLECTION_USERS).document(auth.getCurrentUser().getUid()).set(data_map);
+                }else {
+                    Toast.makeText(RegistroActivity.this, "No se ha podido crear el usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private boolean checkMail() {
 
-        if (edit_email.getText().toString().matches(".*@.*\\..*")) {
-            Toast.makeText(RegistroActivity.this, "mail bien", Toast.LENGTH_SHORT).show();
+        if (edit_email.getText().toString().matches(".*@.*\\..*[{a-z}]")) {
+            //Toast.makeText(RegistroActivity.this, "mail bien", Toast.LENGTH_SHORT).show();
             return true;
         }else {
-            Toast.makeText(RegistroActivity.this, "mail mal", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(RegistroActivity.this, "mail mal", Toast.LENGTH_SHORT).show();
             return false;
+        }
+
+    }
+
+    private void checkFields() {
+
+        if (checkMail() && checkUser() && checkPassword()) {
+            btn_register.setEnabled(true);
+            //Toast.makeText(RegistroActivity.this, "activo", Toast.LENGTH_SHORT).show();
+        } else {
+            btn_register.setEnabled(false);
         }
 
     }
@@ -76,11 +173,11 @@ public class RegistroActivity extends AppCompatActivity {
     }
 
     private boolean checkPassword() {
-        if (edit_password.getText().toString().equals(edit_password_confirm.getText().toString())) {
-            Toast.makeText(RegistroActivity.this, "contra bein", Toast.LENGTH_SHORT).show();
+        if (edit_password.getText().toString().equals(edit_password_confirm.getText().toString()) && !(edit_password.getText().toString().equals("") && edit_password_confirm.getText().toString().equals(""))) {
+            //Toast.makeText(RegistroActivity.this, "contra bein", Toast.LENGTH_SHORT).show();
             return true;
         }else {
-            Toast.makeText(RegistroActivity.this, "contra mal", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(RegistroActivity.this, "contra mal", Toast.LENGTH_SHORT).show();
             return false;
         }
 
