4a652c6ee643179973182b2f442f12dec5427d67
plaza19 <diegoplazarodan19@protonmail.ch>
2021-05-01 14:54:57 +0200

    Reset password work

--- app/src/main/java/com/plaza19/sharelife/MainActivity.java
+++ app/src/main/java/com/plaza19/sharelife/MainActivity.java
@@ -7,6 +7,8 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -80,5 +82,6 @@ public class MainActivity extends AppCompatActivity {
                 
             }
         });
+
     }
 }
